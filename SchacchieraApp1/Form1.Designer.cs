94075a53ba5030e859717792faa4b4d17e08eaf8
Maria Ferrari <maria.ferrari.studenti@isii.it>
2020-11-19 10:19:23 +0100

    Aggiungere i file di progetto.

---
+++ SchacchieraApp1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SchacchieraApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scacchieraControl1 = new SchacchieraApp1.ScacchieraControl();
+            this.SuspendLayout();
+            // 
+            // scacchieraControl1
+            // 
+            this.scacchieraControl1.Lato = 46;
+            this.scacchieraControl1.Location = new System.Drawing.Point(205, 41);
+            this.scacchieraControl1.Name = "scacchieraControl1";
+            this.scacchieraControl1.Size = new System.Drawing.Size(370, 369);
+            this.scacchieraControl1.TabIndex = 0;
+            this.scacchieraControl1.Text = "scacchieraControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scacchieraControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScacchieraControl scacchieraControl1;
+    }
+}
+
---
+++ SchacchieraApp1/ScacchieraControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchacchieraApp1
+{
+    class ScacchieraControl:Control
+    {
+        int lato;
+        public int Lato
+        {
+            get { return lato; }
+            set { lato = value; Refresh(); }
+        }
+        public ScacchieraControl()
+        {
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var g = e.Graphics;
+            //decido il lato della casella (1/8 della dimensione minore)
+            if (Width < Height)
+                lato = Width / 8;
+            else lato = Height / 8;
+            for(int i=0; i<8; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    //assegno x e y del punto da cui iniziare a fare il quadrato
+                    int x = i * Lato;
+                    int y = j * Lato;
+                    //se sia riga che colonna sono pari o dispari ci metto il colore chiaro
+                    if (j % 2 == 1 && i % 2 == 1 || j % 2 == 0 && i % 2 == 0)  
+                        g.FillRectangle(Brushes.LightPink, new Rectangle(x, y, Lato, Lato));
+                    else
+                        //altrimenti quello scuro <3
+                        g.FillRectangle(Brushes.HotPink, new Rectangle(x, y, Lato, Lato));
+                }
+            }
+            //per le stringhe devo ancora pensarlo un attimo uwu
+        }
+    }
+}
